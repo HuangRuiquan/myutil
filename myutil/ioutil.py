9ac29e791b646bd7a38b9b5ebb97588f48a42532
Huang Ruiquan <2510853126@qq.com>
2023-03-25 14:59:16 +0800

    ..

--- myutil/ioutil.py
+++ myutil/ioutil.py
@@ -1,13 +1,39 @@
 import os
 
-__all__ = []
+__all__ = [
+    "Path"
+]
+
+from typing import List
 
 
-def get_one_sub_folder_list(path):
-    path_list = os.listdir(path)
-
-    print(path_list)
+class FooError(ValueError):
+    pass
 
 
-if __name__ =="__main__":
-    get_one_sub_folder_list(r"D:\python\myutil\tests\folder")
+class Path:
+    def __init__(self, path):
+        """
+        用于操作跟路径有关的类
+        :param path: 路径
+        """
+        self.path = path
+
+    def get_one_sub_folder_list(self) -> List[str]:
+        """
+        根据提供的路径返回路径下的子文件夹列表
+        :return:
+        """
+        if not os.path.isdir(self.path):
+            raise FooError(f"提供的路径不存在或不是文件夹:{self.path}")
+        path_list = [os.path.join(self.path, p) for p in os.listdir(self.path)]
+        folder_list = []
+        for p in path_list:
+            if os.path.isdir(p):
+                folder_list.append(p)
+        return folder_list
+
+
+if __name__ == "__main__":
+
+    print(Path(r"D:\python\myutil\tests\folder").get_one_sub_folder_list())
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,13 @@
 # Sample Test passing with nose and pytest
+from myutil.ioutil import Path
 
 def test_pass():
     assert True, "dummy sample test"
+
+
+def test_Path_get_one_sub_folder_list():
+    return Path(r"folder").get_one_sub_folder_list() == [
+                                                        r"D:\python\myutil\tests\folder\folder_1_1",
+                                                        r"D:\python\myutil\tests\folder\folder_1_2",
+                                                        r"D:\python\myutil\tests\folder\folder_1_3"
+                                                         ]
