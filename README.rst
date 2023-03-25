4bfdb51a8e46e48a290f162ffd361bd96602313e
Huang Ruiquan <2510853126@qq.com>
2023-03-25 11:46:16 +0800

    首次提交

---
+++ README.rst
@@ -0,0 +1,32 @@
+myutil
+======
+
+.. image:: https://img.shields.io/pypi/v/myutil.svg
+    :target: https://pypi.python.org/pypi/myutil
+    :alt: Latest PyPI version
+
+.. image:: https://travis-ci.org/HuangRuiquan/myutil.png
+   :target: https://travis-ci.org/HuangRuiquan/myutil
+   :alt: Latest Travis CI build status
+
+对一些包包装成个人使用的包
+
+Usage
+-----
+
+Installation
+------------
+
+Requirements
+^^^^^^^^^^^^
+
+Compatibility
+-------------
+
+Licence
+-------
+
+Authors
+-------
+
+`myutil` was written by `HRQ <2510853126@qq.com>`_.
---
+++ setup.py
@@ -0,0 +1,42 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="myutil",
+    version="0.1.0",
+    url="https://github.com/HuangRuiquan/myutil",
+
+    author="HRQ",
+    author_email="2510853126@qq.com",
+
+    description="对一些包包装成个人使用的包",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
