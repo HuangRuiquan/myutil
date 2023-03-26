e4fae103f70c4e1cd1474346e4d66f7e98835768
Huang Ruiquan <2510853126@qq.com>
2023-03-26 21:09:17 +0800

    amend bug

--- myutil/ioutil.py
+++ myutil/ioutil.py
@@ -40,16 +40,18 @@ class Path:
         :param suffix:指定文件后缀名列表
         :return: 返回文件列表
         """
-        if suffix is None:
-            if not os.path.isdir(self.path):
-                raise FooError(f"提供的路径不存在或者不是文件夹:{self.path}")
-            path_list = [os.path.join(self.path, p) for p in os.listdir(self.path)]
-            folder_list = []
-            for p in path_list:
-                if os.path.isfile(p):
+        if not os.path.isdir(self.path):
+            raise FooError(f"提供的路径不存在或者不是文件夹:{self.path}")
+        path_list = [os.path.join(self.path, p) for p in os.listdir(self.path)]
+        folder_list = []
+        for p in path_list:
+            if os.path.isfile(p):
+                if suffix is None:
                     folder_list.append(p)
-            return folder_list
-        # todo 根据后缀名返回规定列表
+                else:
+                    if os.path.splitext(p)[1] in suffix:
+                        folder_list.append(p)
+        return folder_list
 
     def get_one_sub_folder_name_list(self) -> List[str]:
         """
@@ -63,12 +65,44 @@ class Path:
         return folder_name_list
 
     def get_file_name(self, retain_suffix=True) -> str:
+        """
+        获取文件名，可以指定是否带有后缀名，默认带有后缀
+        :param retain_suffix: 是否带后缀
+        :return: 返回文件名
+        """
         if retain_suffix:
             return os.path.basename(self.path)
         else:
             return os.path.splitext(os.path.basename(self.path))[0]
 
+    def get_all_sub_folder_list(self) -> List[str]:
+        """
+        获取文件夹下所有子文件夹绝对路径
+        :return: 子文件夹绝对路径列表
+        """
+        folder_list = self.get_sub_folder_list()
+        for folder in folder_list:
+            f_list = Path(folder).get_sub_folder_list()
+            if not f_list.__len__():
+                continue
+            else:
+                folder_list.extend(f_list)
+                Path(folder).get_all_sub_folder_list()
+        return folder_list
+
+    def get_all_file_list(self, suffix: List[str] = None) -> List[str]:
+        """
+        获取当前文件夹及所有子文件夹下的文件的绝对路径
+        :param suffix: 文件后缀名列表
+        :return: 返回文件的绝对路径列表
+        """
+        folder_list = self.get_all_sub_folder_list()
+        file_list = self.get_file_list_at_present_dir(suffix)
+        for folder in folder_list:
+            file_list.extend(Path(folder).get_file_list_at_present_dir(suffix))
+        return file_list
+
 
 if __name__ == "__main__":
-
-    print(Path(r"D:\python\myutil\tests\folder\file_0_1.txt").get_file_name())
+    print(Path(r"D:\python\myutil\tests\folder").get_all_file_list([".png", ".rtf"]).__len__())
+    print(Path(r"D:\python\myutil\tests\folder").get_all_file_list([".png", ".rtf"]))
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,30 +3,38 @@ from myutil.ioutil import Path
 
 
 def test_pass():
-    assert test_path_get_one_sub_folder_list()\
-           and test_path_get_one_sub_folder_name_list()\
-           and test_path_get_file_name(), "class_Path test Fail"
+    assert test_path_get_sub_folder_list(), "path_get_sub_folder_list test Fail"
+    assert test_path_get_one_sub_folder_name_list(), "path_get_one_sub_folder_name_list test Fail"
+    assert test_path_get_file_name(), "path_get_file_name test Fail"
+    assert test_path_get_all_sub_folder_list(), "path_get_all_sub_folder_list test Fail"
+    assert test_path_get_all_file_list(), "path_get_all_file_list test Fail"
+    print("simple test pass")
 
 
-def test_path_get_one_sub_folder_list():
-    return Path(r"folder").get_sub_folder_list() == [
-        r"D:\python\myutil\tests\folder\folder_1_1",
-        r"D:\python\myutil\tests\folder\folder_1_2",
-        r"D:\python\myutil\tests\folder\folder_1_3"
-    ]
+def test_path_get_sub_folder_list():
+    return Path(r"folder").get_sub_folder_list().__len__() == 4
 
 
 def test_path_get_one_sub_folder_name_list():
-    return Path(r"folder").get_one_sub_folder_name_list() == [
-        r"folder_1_1",
-        r"folder_1_2",
-        r"folder_1_3"
-    ]
+    return Path(r"folder").get_one_sub_folder_name_list() == \
+           ['file_0_3.4.txt', 'folder_1_1', 'folder_1_2', 'folder_1_3']
 
 
 def test_path_get_file_name():
     return Path(r"folder/file_0_1.txt").get_file_name() == "file_0_1.txt" and \
            Path(r"folder/file_0_1.txt").get_file_name(False) == "file_0_1"
+
+
+def test_path_get_all_sub_folder_list():
+    return Path(r"folder").get_all_sub_folder_list().__len__() == 11
+
+
+def test_path_get_all_file_list():
+    """
+    测试Path get_all_file_list方法跟get_file_list_at_present_dir 方法
+    :return:
+    """
+    return Path(r"folder").get_all_file_list([".png", ".rtf"]).__len__() == 2
 
 
 if __name__ == "__main__":
